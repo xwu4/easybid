dc4e23fd31b9507708d898ca77537f032ae5afe4
xwu4 <78444760+xwu4@users.noreply.github.com>
2021-04-30 01:29:22 -0400

    redis again

--- webapps/asgi.py
+++ webapps/asgi.py
@@ -10,22 +10,22 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import get_default_application
-# from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from django.core.asgi import get_asgi_application
 import chat.routing
 import django
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings')
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             chat.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings')
-django.setup()
-application = get_default_application()
+
+application = ProtocolTypeRouter({
+    "http": get_default_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings')
+# django.setup()
+# application = get_default_application()
