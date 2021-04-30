63726a8359e1a4424510f98fb9c5be0dd6f56583
xwu4 <78444760+xwu4@users.noreply.github.com>
2021-04-30 15:12:08 -0400

    redis finally working

--- chat/consumers.py
+++ chat/consumers.py
@@ -33,35 +33,28 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # id = text_data_json['id']
-        # username = text_data_json['username']
-        # price = float(message)
-        # status, error_message = bid_price_check(id, price)
-        # # Send message to room group
-        # if status == True:
-        #     async_to_sync(self.channel_layer.group_send)(
-        #         self.room_group_name,
-        #         {
-        #             'type': 'chat_message',
-        #             'message': message,
-        #             'username': username
-        #         }
-        #     )
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-            }
-        )
+        id = text_data_json['id']
+        username = text_data_json['username']
+        price = float(message)
+        status, error_message = bid_price_check(id, price)
+        # Send message to room group
+        if status == True:
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'username': username
+                }
+            )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
-        # username = event['username']
+        username = event['username']
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message,
-            # 'username': username,
+            'username': username,
         }))
