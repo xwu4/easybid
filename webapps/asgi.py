a6ccc6b5d905ec07954f0dfe84f6aeaccea310e0
xwu4 <78444760+xwu4@users.noreply.github.com>
2021-04-30 02:36:04 -0400

    redis again

--- webapps/asgi.py
+++ webapps/asgi.py
@@ -27,6 +27,6 @@ import chat.routing
 # })
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings')
-import django
-django.setup()
+# import django
+# django.setup()
 application = get_default_application()
