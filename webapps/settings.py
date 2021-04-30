3ad359b4153d9fdb9454a0bcae2c0a048762673e
xwu4 <78444760+xwu4@users.noreply.github.com>
2021-04-29 22:23:56 -0400

    Update settings.py

--- webapps/settings.py
+++ webapps/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from configparser import ConfigParser
+from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
